6172838b81ce3aabdc25d79dea5dd8bffae337f8
ryan <ryan.kimani0@gmail.com>
2023-05-29 09:16:53 +0300

    Completed Assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    end_with?(".")
   end
 
   def question?
+    end_with?("?")
 
   end
 
   def exclamation?
+    end_with?("!")
 
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
